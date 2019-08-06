54d208fa567cdd5bb2d2449a3954b7bf0f563c77
satar619 <abdulsatarsherzai300@gmail.com>
2019-08-06 11:18:23 +0200

    box test

---
+++ box/js/data.js
@@ -0,0 +1,37 @@
+export const data = [{
+        name: 'Ines Julia Rockmann',
+        img: 'https://www.freelancer-rheinmain.de/profile/ines-julia-rockmann',
+        posation: 'Grafikdesign Print & Web',
+        skils: {
+            fist: 'Corporate Design',
+            second: 'Editorial Design',
+        }
+    },
+    {
+        name: 'Ines Julia Rockmann',
+        img: 'https://www.freelancer-rheinmain.de/profile/ines-julia-rockmann',
+        posation: 'Grafikdesign Print & Web',
+        skils: {
+            fist: 'Corporate Design',
+            second: 'Editorial Design',
+        }
+    },
+    {
+        name: 'Ines Julia Rockmann',
+        img: 'https://www.freelancer-rheinmain.de/profile/ines-julia-rockmann',
+        posation: 'Grafikdesign Print & Web',
+        skils: {
+            fist: 'Corporate Design',
+            second: 'Editorial Design',
+        }
+    },
+    {
+        name: 'Ines Julia Rockmann',
+        img: 'https://www.freelancer-rheinmain.de/profile/ines-julia-rockmann',
+        posation: 'Grafikdesign Print & Web',
+        skils: {
+            fist: 'Corporate Design',
+            second: 'Editorial Design',
+        }
+    },
+];
