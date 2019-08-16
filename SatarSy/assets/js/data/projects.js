5e8c54f6b179f14ab7dfc847f583a8e2e114cd9b
SatarSy <abdulsatarsherzai300@gmail.com>
2019-08-16 15:50:03 +0200

    test

---
+++ SatarSy/assets/js/data/projects.js
@@ -0,0 +1,7 @@
+const projects = [{
+    name: 'Banana',
+    img: '', // TO DO= img URL
+    description: '', // TO DO= description of the project
+    host: '', // TO DO= link to the host web
+    code: '' // TO DO= link to the code of the project
+}];
